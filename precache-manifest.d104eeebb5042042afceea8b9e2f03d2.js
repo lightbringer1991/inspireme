b93e3187139a6c0dbdf8e60f16b572ae2eb3f22e
Tuan Nguyen <lightbringer1991@yahoo.com>
2020-10-23 19:21:04 +1100

    Updates

---
+++ precache-manifest.d104eeebb5042042afceea8b9e2f03d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125e5310dd004962bc34b2fab1b018ac",
+    "url": "/inspireme/index.html"
+  },
+  {
+    "revision": "aca99e0ff04ae279169c",
+    "url": "/inspireme/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "043b6b1cd33d9f13190b",
+    "url": "/inspireme/static/css/main.172ee628.chunk.css"
+  },
+  {
+    "revision": "aca99e0ff04ae279169c",
+    "url": "/inspireme/static/js/2.8aba9353.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/inspireme/static/js/2.8aba9353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043b6b1cd33d9f13190b",
+    "url": "/inspireme/static/js/main.1053b042.chunk.js"
+  },
+  {
+    "revision": "6d778f1624a9acf98109",
+    "url": "/inspireme/static/js/runtime-main.7cb0a8e8.js"
+  }
+]);
